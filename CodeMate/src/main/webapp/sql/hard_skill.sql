2656f5c937ba28608649fd022fbe7efae329f464
reumeum <boreum1111@gmail.com>
2024-05-23 23:59:40 +0900

    hard skill logo/sql update (미완!)

--- CodeMate/src/main/webapp/sql/hard_skill.sql
+++ CodeMate/src/main/webapp/sql/hard_skill.sql
@@ -8,36 +8,47 @@ INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.ne
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Go', 1);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'C', 1);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'C++', 1);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Rust', 1);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Scala', 1);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Kotlin', 1);
 
 
 -- 백엔드 프레임워크 (Backend Frameworks)
-INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Spring Framework', 1);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Spring', 1);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Django', 1);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Flask', 1);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Express.js', 1);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Laravel', 1);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Ruby on Rails', 1);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'ASP.NET', 1);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Ktor', 1);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Micronaut', 1);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Quarkus', 1);
 
 
 -- 프론트엔드 (Frontend)
 -- 언어
-INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'HTML', 2);
-INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'CSS', 2);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'HTML5', 2);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'CSS3', 2);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'JavaScript', 2);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'TypeScript', 2);
 
 -- 프레임워크
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'React.js', 2);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Angular', 2);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Vue.js', 2);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Svelte', 2);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Alpine.js', 2);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Ember.js', 2);
 
 -- 그 외 기술
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Sass', 2);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Less', 2);
-INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, '웹 디자인 및 사용자 경험 (UI/UX)', 2);
-INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, '웹 접근성 (Accessibility)', 2);
-INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, '반응형 및 적응형 웹 디자인', 2);
-INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, '웹 성능 최적화', 2);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'WebAssembly (Wasm)', 2);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'GraphQL', 2);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'WebSocket', 2);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'PWA (Progressive Web Apps) 개발', 2);
+
 
 
 -- 데이터베이스 (Database)
@@ -48,9 +59,16 @@ INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.ne
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'MongoDB', 3);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Redis', 3);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Cassandra', 3);
-INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, '데이터베이스 설계 및 모델링', 3);
-INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, '데이터베이스 관리 및 운영', 3);
-INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, '데이터베이스 보안 및 백업/복원', 3);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'MariaDB', 3);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'CouchDB', 3);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Neo4j', 3);
+
+
+----------여기까지 로고 다운 완료
+
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Elasticsearch', 3);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'InfluxDB', 3);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'TimescaleDB', 3);
 
 
 -- 모바일 (Mobile)
@@ -62,6 +80,10 @@ INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.ne
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'React Native', 4);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Flutter', 4);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Xamarin', 4);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Ionic', 4);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'NativeScript', 4);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'SwiftUI', 4);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Jetpack Compose', 4);
 
 
 -- 인프라 (Infrastructure)
@@ -73,10 +95,17 @@ INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.ne
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Prometheus', 5);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Grafana', 5);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'ELK Stack (Elasticsearch, Logstash, Kibana)', 5);
-INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Firewall 설정', 5);
-INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'VPN 구성', 5);
-INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'IDS/IPS (Intrusion Detection/Prevention System)', 5);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Bash Script', 5);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'PowerShell', 5);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Python Script', 5);
 INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'github', 5);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Terraform', 5);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Ansible', 5);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Chef', 5);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Puppet', 5);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Jenkins', 5);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'GitLab CI/CD', 5);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Nginx', 5);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Apache HTTP Server', 5);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'CloudFormation', 5);
+INSERT INTO hard_skill (hs_code, hs_name, hs_category) VALUES (hard_skill_seq.nextval, 'Cloud Functions', 5);
