669e2895623fd225847f20d5ff1fbfe8aa2150aa
KimGryut <wtk0912@naver.com>
2024-05-24 15:23:52 +0900

    .

---
+++ CodeMate/src/main/java/kr/board/action/CommunityAction.java
@@ -0,0 +1,16 @@
+package kr.board.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.controller.Action;
+
+public class CommunityAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// 커뮤니티 (자유 게시판으로 이동)
+		return "/WEB-INF/views/cBoard/freeBoard.jsp";
+	}
+
+}
---
+++ CodeMate/src/main/java/kr/board/action/CommunityAction2.java
@@ -0,0 +1,16 @@
+package kr.board.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.controller.Action;
+
+public class CommunityAction2 implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// 커뮤니티 (개발 게시판으로 이동)
+		return "/WEB-INF/views/cBoard/codingBoard.jsp";
+	}
+
+}
