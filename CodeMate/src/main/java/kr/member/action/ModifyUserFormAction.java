8dc21ff2adddef2bcf18fa54a9e54be82ea28407
boonmojae <pminny99@naver.com>
2024-05-24 16:01:03 +0900

    Merge branch 'main' of https://github.com/reumeum/CodeMate.git into main

--- CodeMate/src/main/java/kr/member/action/ModifyUserFormAction.java
+++ CodeMate/src/main/java/kr/member/action/ModifyUserFormAction.java
@@ -8,21 +8,25 @@ import kr.controller.Action;
 import kr.member.dao.MemberDAO;
 import kr.member.vo.MemberVO;
 
-public class ModifyUserFormAction implements Action{
+public class ModifyUserFormAction implements Action {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		/*
-		 * //세션에 로그인된 상태 HttpSession session = request.getSession(); //로그인 체크 Integer
-		 * user_num = (Integer)session.getAttribute("user_num"); if(user_num==null)
-		 * {//로그인이 되지 않은 경우 return "redirect:/member/loginForm.do"; } //로그인이 된 경우
-		 * MemberDAO dao = MemberDAO.getInstance(); MemberVO member =
-		 * dao.getMember(user_num);
-		 * 
-		 * request.setAttribute("member", member);
-		 */
+		//세션에 로그인된 상태로 있어야됨
+		HttpSession session = request.getSession();
+		//로그인 체크
+		Integer mem_num = (Integer)session.getAttribute("mem_num");
+		if(mem_num == null) {//로그인이 되지 않은 경우
+			return "redirect:/member/loginForm.do";
+		}
+		
+		//로그인이 된 경우
+		MemberDAO dao = MemberDAO.getInstance();
+		MemberVO member = dao.getMember(mem_num);//한건의 데이터 읽어옴
+		
+		request.setAttribute("member", member);
 		//JSP 경로 반환
-		return "/WEB-INF/views/member/modifyUserForm.jsp";
+		return "/WEB-INF/views/member/modifyUserForm.jsp";//forward할 경우 내부에 있을땐 jsp
 	}
 
 }
