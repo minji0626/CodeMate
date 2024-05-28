ca29e53e282013dc349ec0c033d767cf011ca69e
boonmojae <pminny99@naver.com>
2024-05-28 17:50:44 +0900

    minjae

--- CodeMate/src/main/java/kr/member/action/MyPageMoAction.java
+++ CodeMate/src/main/java/kr/member/action/MyPageMoAction.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import kr.controller.Action;
+import kr.member.dao.MemberDAO;
+import kr.member.vo.MemberVO;
 import kr.rboard.dao.RboardDAO;
 import kr.rboard.vo.RboardVO;
 
@@ -21,14 +23,21 @@ public class MyPageMoAction implements Action{
 			
 		}
 		//로그인 된 경우
-		RboardDAO rdao = RboardDAO.getInstance();
-		RboardVO rboard = rdao.getrboard(mem_num);
+		MemberDAO dao = MemberDAO.getInstance();
+		MemberVO member = dao.getMember(mem_num);
+		//list에 반환된 데이터 정보를 띄우는거니까 상세보기 getboad가 지금 여기서 안필요함??
+		/*
+		 * 
+		 * RboardVO rboard = rdao.getrboard(mem_num);
+		 */
 		
-		//내가 쓴 모집글 메서드 불러오기 rboar에서 
+		//내가 쓴 모집글 메서드 불러오기 rboard에서 
+		RboardDAO rboardDAO = RboardDAO.getInstance(); 
+		//list 반환
+		//request.setAttribute("", rboardDAO)
 		
 		
-		
-		request.setAttribute("rboard", rboard);
+		//request.setAttribute("rboard", rboard);
 		
 		//JSP 경로 반환
 		return "/WEB-INF/views/member/myPageMo.jsp";
