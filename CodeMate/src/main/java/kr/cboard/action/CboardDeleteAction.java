151e48533a563144a8c1d1620ade3d744d570841
KimGryut <wtk0912@naver.com>
2024-06-04 15:33:33 +0900

    .

--- CodeMate/src/main/java/kr/cboard/action/CboardDeleteAction.java
+++ CodeMate/src/main/java/kr/cboard/action/CboardDeleteAction.java
@@ -4,47 +4,50 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.tomcat.dbcp.dbcp2.datasources.SharedPoolDataSource;
-
 import kr.cboard.dao.CboardDAO;
 import kr.cboard.vo.CboardVO;
 import kr.controller.Action;
 import kr.util.FileUtil;
 
-public class CboardDeleteAction implements Action{
+public class CboardDeleteAction implements Action {
 
-	@Override
-	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		HttpSession session = request.getSession();
-		Integer mem_num = 
-				(Integer)session.getAttribute("mem_num");
-		if(mem_num == null) {//로그인이 되지 않은 경우
-			return "redirect:/member/loginForm.do";
-		}
-		//로그인 된 경우
-		int cb_num = Integer.parseInt(request.getParameter("cb_num"));
-		System.out.println(cb_num);
-		CboardDAO dao = CboardDAO.getInstance();
-		CboardVO db_board = dao.detailCboard(cb_num);
-		//로그인한 회원번호와 작성자 회원번호 일치 여부 체크
-		if(mem_num != db_board.getMem_num()) {
-			return "/WEB-INF/views/common/notice.jsp";
-		}
-		
-		//로그인한 회원번호와 작성자 회원번호 일치
-		dao.deleteCboard(cb_num);
-		//파일 삭제
-		FileUtil.removeFile(request, db_board.getCb_file());
-		
-		request.setAttribute("notice_msg", "글 삭제 완료");
-		request.setAttribute("notice_url", 
-				request.getContextPath()+"/cboard/community.do?cb_type=0");
-		
-		return "/WEB-INF/views/common/alert_view.jsp";
-	}
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        HttpSession session = request.getSession();
+        Integer mem_num = (Integer) session.getAttribute("mem_num");
+        
+        if (mem_num == null) { // 로그인이 되지 않은 경우
+            return "redirect:/member/loginForm.do";
+        }
 
+        // 로그인 된 경우
+        int cb_num = Integer.parseInt(request.getParameter("cb_num"));
+        CboardDAO dao = CboardDAO.getInstance();
+        CboardVO db_board = dao.detailCboard(cb_num);
+
+        Integer mem_auth = (Integer) session.getAttribute("mem_auth");
+        
+        if(mem_auth==9) { // 관리자
+        	 dao.deleteCboard(cb_num);
+             // 파일 삭제
+             FileUtil.removeFile(request, db_board.getCb_file());
+             
+             request.setAttribute("notice_msg", "글 삭제 완료");
+             request.setAttribute("notice_url", request.getContextPath() + "/cboard/community.do?cb_type=0");
+             return "/WEB-INF/views/common/alert_view.jsp";
+        } else if (!mem_num.equals(db_board.getMem_num())) { // 로그인한 회원번호와 작성자 회원번호 일치 여부 체크
+            request.setAttribute("notice_msg", "작성자만 삭제할 수 있습니다.");
+            request.setAttribute("notice_url", request.getContextPath() + "/cboard/communityDetail.do?cb_num=" + cb_num);
+            return "/WEB-INF/views/common/alert_view.jsp";
+        } else {
+            // 로그인한 회원번호와 작성자 회원번호 일치
+            dao.deleteCboard(cb_num);
+            // 파일 삭제
+            FileUtil.removeFile(request, db_board.getCb_file());
+            
+            request.setAttribute("notice_msg", "글 삭제 완료");
+            request.setAttribute("notice_url", request.getContextPath() + "/cboard/community.do?cb_type=0");
+            return "/WEB-INF/views/common/alert_view.jsp";
+        }
+    }
 }
-
-
-
-
---
+++ CodeMate/src/main/java/kr/member/action/ActivationAction.java
@@ -0,0 +1,48 @@
+package kr.member.action;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import kr.controller.Action;
+import kr.rboard.dao.ApplyDAO;
+import kr.rboard.vo.RapplyVO;
+
+public class ActivationAction  implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		Map<String, String> mapAjax = new HashMap<String, String>();
+
+		// 세션에 로그인된 상태로 있어야 됨
+		HttpSession session = request.getSession();
+
+		// 로그인 체크
+		Integer login_num = (Integer)session.getAttribute("mem_num");
+		if(login_num == null) { // 로그인이 되지 않은 경우
+			request.setAttribute("notice_msg","로그인 하세요!");
+			request.setAttribute("notice_url", "redirect:/member/loginForm.do");
+			return "/WEB-INF/views/common/alert_view.jsp";
+		} 
+
+		request.setCharacterEncoding("utf-8");
+
+		int rb_num = Integer.parseInt(request.getParameter("rb_num"));
+
+		ApplyDAO dao = ApplyDAO.getInstance();
+
+		dao.teamActivation(rb_num);
+		request.setAttribute("notice_msg","프로젝트가 활성화되었습니다!");
+		request.setAttribute("notice_url", request.getContextPath()+"/member/myPageMoShin.do?rb_num="+rb_num);
+		return "/WEB-INF/views/common/alert_view.jsp";
+
+
+
+	}
+
+
+}
