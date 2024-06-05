96cb48105a240307f3ad2f4e8ecafdb9c5cb3e22
minji0626 <jo010163@naver.com>
2024-06-05 12:07:00 +0900

    team_num 링크로 변경해서 접근 불가능하도록 변경

--- CodeMate/src/main/java/kr/team/action/TeamSettingAction.java
+++ CodeMate/src/main/java/kr/team/action/TeamSettingAction.java
@@ -1,5 +1,6 @@
 package kr.team.action;
 
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -7,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import kr.controller.Action;
+import kr.member.dao.TeamDAO;
+import kr.member.vo.TeamVO;
 import kr.tmember.dao.TmemberDAO;
 import kr.tmember.vo.TmemberVO;
 
@@ -14,15 +17,50 @@ public class TeamSettingAction implements Action{
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		request.setCharacterEncoding("utf-8");
+		
 		HttpSession session = request.getSession();
 		Integer mem_num = (Integer)session.getAttribute("mem_num");
-		int team_num = Integer.parseInt(request.getParameter("team_num"));
+		TeamDAO tdao = TeamDAO.getInstance();
+		Integer team_num = (Integer)session.getAttribute("team_num");
+		
+		int team_num2 = Integer.parseInt(request.getParameter("team_num"));
+		
 		
 		if(mem_num == null) {// 로그인 미실시
 			return "redirect:/member/loginForm.do";
 		}
-		request.setCharacterEncoding("utf-8");
-		TmemberDAO dao = TmemberDAO.getInstance();
+		
+		
+		TeamVO teams = tdao.getTeam(team_num2);
+		if (teams == null || teams.getTeam_status() == 0) {
+		    // 팀이 존재하지 않는 경우
+		    response.setContentType("text/html; charset=UTF-8");
+		    PrintWriter out = response.getWriter();
+		    out.println("<script type=\"text/javascript\">");
+		    out.println("var contextPath = '" + request.getContextPath() + "';");
+		    out.println("alert('팀이 존재하지 않거나 비활성화 팀입니다. 참여중인 팀 페이지로 돌아갑니다.');");
+		    out.println("window.location.href = contextPath + '/member/myTeam.do';");
+		    out.println("</script>");
+		    return null;
+		}
+		
+		TeamVO teammember = tdao.getTeamMember(mem_num, team_num2);
+	        if(teammember == null) {
+	        	response.setContentType("text/html; charset=UTF-8");
+	        	PrintWriter out = response.getWriter();
+	        	out.println("<script type=\"text/javascript\">");
+	        	out.println("var contextPath = '" + request.getContextPath() + "';");
+	        	out.println("alert('잘못된 접근입니다. 참여중인 팀으로 돌아갑니다.');");
+	        	out.println("window.location.href = contextPath + '/member/myTeam.do';");
+	        	out.println("</script>");
+
+	            return null;
+	        } 
+		
+		
+	    TmemberDAO dao = TmemberDAO.getInstance();
 		int count = dao.getTmemberCount(team_num);
 		
 		List<TmemberVO> list = null;
