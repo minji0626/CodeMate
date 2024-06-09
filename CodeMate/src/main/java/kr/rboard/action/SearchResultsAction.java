fa00cd95ec99314a8c1805fd6061e914d5fe70b9
reumeum <boreum1111@gmail.com>
2024-06-09 22:08:15 +0900

    rboardlist 검색 동작 오류 수정

--- CodeMate/src/main/java/kr/rboard/action/SearchResultsAction.java
+++ CodeMate/src/main/java/kr/rboard/action/SearchResultsAction.java
@@ -30,17 +30,17 @@ public class SearchResultsAction implements Action {
 		if (rowCount == null) {
 			rowCount = "12";
 		}
-		
-        // 검색 조건을 받음
-        String[] r_skills = request.getParameterValues("r_skills");
-        String rb_category = request.getParameter("rb_category");
-        String r_fields = request.getParameter("r_fields");
-        String rb_meet = request.getParameter("rb_meet");
-        String search_key = request.getParameter("search_key");
-        boolean recruiting_filter = Boolean.parseBoolean(request.getParameter("recruiting_filter"));
 
-        
-        // 검색 조건을 이용해 데이터베이스에서 결과를 조회
+		// 검색 조건을 받음
+		String[] r_skills = request.getParameterValues("r_skills[]");
+		String rb_category = request.getParameter("rb_category");
+		String r_fields = request.getParameter("r_fields");
+		String rb_meet = request.getParameter("rb_meet");
+		String search_key = request.getParameter("search_key");
+		boolean recruiting_filter = Boolean.parseBoolean(request.getParameter("recruiting_filter"));
+
+
+		// 검색 조건을 이용해 데이터베이스에서 결과를 조회
 		RboardDAO rdao = RboardDAO.getInstance();
 		int count = rdao.getRboardCount(r_skills, rb_category, r_fields, rb_meet, search_key, recruiting_filter);
 
@@ -48,27 +48,25 @@ public class SearchResultsAction implements Action {
 		PagingUtil page = new PagingUtil(Integer.parseInt(pageNum), count, Integer.parseInt(rowCount));
 		List<RboardVO> rboardList = null;
 		if (count > 0) {
-			rboardList = rdao.getRboardList(page.getStartRow(), page.getEndRow(), 
-					r_skills, rb_category, r_fields, rb_meet, search_key, recruiting_filter);
+			rboardList = rdao.getRboardList(page.getStartRow(), page.getEndRow(), r_skills, rb_category, r_fields,
+					rb_meet, search_key, recruiting_filter);
 		} else {
 			rboardList = Collections.emptyList();
 		}
-        
-        
-		//Map 생성
+
+		// Map 생성
 		Map<String, Object> mapAjax = new HashMap<String, Object>();
 		mapAjax.put("count", count);
 		mapAjax.put("rboardList", rboardList);
-		
+
 		ObjectMapper mapper = new ObjectMapper();
 		String ajaxData = mapper.writeValueAsString(mapAjax);
-		
+
 		request.setAttribute("ajaxData", ajaxData);
 		return "/WEB-INF/views/common/ajax_view.jsp";
-    }
+	}
 
 }
 
 //이제 ajax 쪽으로 넘어가서 그부분 수정하고 js에서 페이지 처리 보여주는 부분도 수정
 //검색어 넣었을 때 잘 되는지
-
