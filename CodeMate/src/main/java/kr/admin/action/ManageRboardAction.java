d63eff1efb935e6f654fba129a8a4e35eee3c33b
KimGryut <wtk0912@naver.com>
2024-06-10 15:09:30 +0900

    ./

--- CodeMate/src/main/java/kr/admin/action/ManageRboardAction.java
+++ CodeMate/src/main/java/kr/admin/action/ManageRboardAction.java
@@ -9,12 +9,17 @@ import javax.servlet.http.HttpSession;
 import kr.admin.dao.AdminDAO;
 import kr.cboard.vo.CboardVO;
 import kr.controller.Action;
+import kr.db.dao.DBDAO;
+import kr.db.vo.FieldVO;
+import kr.db.vo.HardSkillVO;
+import kr.rboard.dao.RboardDAO;
+import kr.rboard.vo.RboardVO;
 import kr.util.PagingUtil;
 
-public class ManageCommunityAction implements Action{
+public class ManageRboardAction implements Action{
 
-	@Override
-	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
 		// 세션에 로그인된 상태로 있어야됨
 		HttpSession session = request.getSession();
@@ -27,46 +32,31 @@ public class ManageCommunityAction implements Action{
 		} else if (mem_auth == 9) { //관리자로 로그인 된 경우
 
 			String pageNum = request.getParameter("pageNum");
-			if(pageNum == null) pageNum = "1";
+			if (pageNum == null)
+				pageNum = "1";
 
 			String keyfield = request.getParameter("keyfield");
 			String keyword = request.getParameter("keyword");
 
-			if ("3".equals(keyfield)) {
-				if ("자유 게시판".equals(keyword)) {
-					keyword = "0";
-				} else if ("개발 게시판".equals(keyword)) {
-					keyword = "1";
-				} else if ("개".equals(keyword)) {
-					keyword = "1";
-				} else if ("개발".equals(keyword)) {
-					keyword = "1";
-				} else if ("자유".equals(keyword)) {
-					keyword = "0";
-				} else if ("자".equals(keyword)) {
-					keyword = "0";
-				} else if ("유".equals(keyword)) {
-					keyword = "0";
-				} else if ("발".equals(keyword)) {
-					keyword = "1";
-				} 
+
+			RboardDAO rdao = RboardDAO.getInstance();
+			int count = rdao.getRboardCountManage(keyfield, keyword);
+
+			// 페이지 처리
+			PagingUtil page = new PagingUtil(keyfield, keyword, Integer.parseInt(pageNum), count, 12, 10, "/admin/manageRboard.do");
+
+			List<RboardVO> rboardList = null;
+			if (count > 0) {
+				rboardList = rdao.getListBoardManage(page.getStartRow(), page.getEndRow(), keyfield, keyword);
 			}
 
-			AdminDAO dao = AdminDAO.getInstance();
-			int count = dao.getCount(keyfield, keyword);
-
-			PagingUtil page = new PagingUtil(keyfield, keyword, Integer.parseInt(pageNum), count, 20,20,"manageCommunity.do");
-
-			List<CboardVO> communityList = null;
-			if(count > 0) {
-				communityList = dao.getListBoard(page.getStartRow(), page.getEndRow(), keyfield, keyword);
-			}
+			request.setAttribute("rboardList", rboardList);
 			request.setAttribute("count", count);
-			request.setAttribute("communityList", communityList);
 			request.setAttribute("page", page.getPage());
 
-			// 커뮤니티 (자유 게시판으로 이동)
-			return "/WEB-INF/views/admin/manageCommunity.jsp";
+
+			return "/WEB-INF/views/admin/manageRboard.jsp";
+			
 		} else { //관리자가 아닌 아이디로 로그인 된 경우
 			request.setAttribute("notice_msg", "페이지 접근 권한이 없습니다.");
 			request.setAttribute("notice_url", request.getContextPath() + "/main/main.do");
